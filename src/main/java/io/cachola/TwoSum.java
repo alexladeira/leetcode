5a396a0034fb0cddcc437bd69a8abc18135e0c37
Alexandre Ladeira <alexandre.ladeira@gmail.com>
2024-02-09 15:49:30 +0000

    TwoSum - O(n) solution

--- src/main/java/io/cachola/TwoSum.java
+++ src/main/java/io/cachola/TwoSum.java
@@ -1,13 +1,17 @@
 package io.cachola;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
     public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
+            int complementTarget = target - nums[i];
+            if (map.containsKey(complementTarget)) {
+                return new int[]{map.get(complementTarget), i};
             }
+            map.put(nums[i], i);
         }
         return null;
     }
